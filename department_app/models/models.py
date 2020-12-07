3bc738717d888d6c9204001b47309f2c3d4610ac
eewig <alosha.alosh@ukr.net>
2020-12-07 20:08:47 +0200

    small models change

--- department_app/models/models.py
+++ department_app/models/models.py
@@ -5,8 +5,8 @@ from .. import db
 
 class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    employees = db.relationship('Employee', backref='employee', lazy=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    employees = db.relationship('Employee', backref='department', lazy=True)
 
     def __repr__(self):
         return f'<Department {self.id} - {self.name}>'
@@ -17,7 +17,7 @@ class Employee(db.Model):
     name = db.Column(db.String(80), nullable=False)
     dob = db.Column(types.Date, nullable=False)
     salary = db.Column(db.Integer, nullable=False)
-    department = db.Column(db.Integer, db.ForeignKey('department.id'),
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'),
         nullable=False)
 
     def __repr__(self):
