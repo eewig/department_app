eeae0381a6e2f96ffd079f19013becf9f5e1b8c5
eewig <alosha.alosh@ukr.net>
2020-12-08 21:17:18 +0200

    made mockups

---
+++ documentation/html_prototype/employee_files/resources/story.js
@@ -0,0 +1,13 @@
+var story = {
+ "pages": [{
+  "image": "employee.png",
+  "image2x": "employee@2x.png",
+  "width": 833,
+  "links": [],
+  "title": "employee",
+  "height": 648
+ }],
+ "resolutions": [2],
+ "title": "employee",
+ "highlightLinks": true
+}
---
+++ documentation/html_prototype/employees_files/resources/story.js
@@ -0,0 +1,13 @@
+var story = {
+ "pages": [{
+  "image": "employees.png",
+  "image2x": "employees@2x.png",
+  "width": 833,
+  "links": [],
+  "title": "employees",
+  "height": 648
+ }],
+ "resolutions": [2],
+ "title": "employees",
+ "highlightLinks": true
+}
