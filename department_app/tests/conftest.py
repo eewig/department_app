fb0afe74068c4b51ba35c81b1d83ffeb8d713837
eewig <alosha.alosh@ukr.net>
2020-12-13 11:39:58 +0200

    tests for rest

--- department_app/tests/conftest.py
+++ department_app/tests/conftest.py
@@ -1,27 +1,49 @@
+import os
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 from department_app import create_app, db
 from department_app.config import TestingConfig
-from department_app.models.db import init_db, drop_db
+# from department_app.models.db_utils import init_db, drop_db
 
 
 @pytest.fixture(scope='session')
 def app():
     app = create_app(TestingConfig)
-
+    if os.getenv('GITLAB_CI'):
+        engine = create_engine(TestingConfig.SQLALCHEMY_DATABASE_URI)
+        if not database_exists(engine.url):
+            create_database(engine.url)
     with app.app_context():
-        # init_db()
-        db.create_all()
-    yield app
-    with app.app_context():
-        db.session.remove()
-        db.drop_all()
-        # drop_db()
+        yield app
+    if os.getenv('GITLAB_CI'):
+        drop_database(engine.url)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def client(app):
-    return app.test_client()
+    with app.app_context():
+        db.create_all()
+        yield app.test_client()
+        db.session.remove()
+        if str(db.engine.url) == TestingConfig.SQLALCHEMY_DATABASE_URI:
+            db.drop_all()
+
+
+def create_test_db(func):
+    def wrapper(*args):
+        engine = create_engine(TestingConfig.SQLALCHEMY_DATABASE_URI)
+        if not database_exists(engine.url):
+            create_database(engine.url)
+        func()
+        drop_database(engine.url)
+    return wrapper
+
+def decorate_if_gitlab(decorator):
+    if os.getenv('GITLAB_CI'):
+        return decorator
+
 
 # @pytest.fixture(scope='module')
 # def client():
--- department_app/tests/test_department.py
+++ department_app/tests/test_department.py
@@ -8,12 +8,14 @@ def test_api_department_error(client):
     assert rv.status_code == 400
     assert rv.get_json() == {'message': 'Data is not valid.'}
 
-def test_api_department_list(client):
-    rv = client.post('/api/department', json={'name': 'test_department'})
-    response = client.get('/api/department')
+def test_department_post(client):
+    response = client.post('/api/department',
+        json={'name': 'test_department'})
+    assert response.status_code == 201
+    assert response.get_json() == {'name': 'test_department'}
 
-    assert rv.status_code == 201
-    assert rv.get_json() == {'name': 'test_department'}
+def test_api_department_list(client):
+    response = client.get('/api/department')
     assert response.get_json() == [{'employees': [],
         'id': 1, 'name': 'test_department'}]
     assert response.status_code == 200
@@ -31,6 +33,19 @@ def test_api_department_get(client):
     assert response.status_code == 200
     assert response.get_json() == {"employees": [], "id": 1,
         "name": "new_department"}
+
+def test_get_departments_avg_salary(client):
+    response = client.post('/api/employee',
+        json={'name': 'Jack', 'dob': '2000-01-01', 'salary': 500,
+        'department_id': 1})
+    response = client.post('/api/employee',
+        json={'name': 'John', 'dob': '2000-01-01', 'salary': 1500,
+        'department_id': 1})
+    response = client.get('/api/department',
+        query_string={'avg': 1})
+    assert response.status_code == 200
+    assert response.get_json() == [{'id': 1, 'name': 'new_department',
+    'average-salary': 1000.0}]
 
 def test_api_department_delete(client):
     response = client.delete('/api/department/1')
