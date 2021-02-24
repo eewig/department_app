87df34d0d157a2cc26070c8c5a96e4da488c706a
Oleksii Sovkov <oleksii_sovkov@epam.com>
2021-02-24 14:59:20 +0200

    deleted unnecessary files

--- build/lib/department_app/rest/__init__.py
+++
@@ -1,7 +0,0 @@
-"""REST API package."""
-
-
-from flask import Blueprint
-
-
-api_bp = Blueprint('api', __name__, url_prefix='/api')
--- build/lib/tests/test_factory.py
+++
@@ -1,4 +0,0 @@
-
-
-def test_config(app):
-    assert app.testing
